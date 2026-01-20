7b59c4d31393b5f88cdf43625da21e69e4ff9b93
Iresha Bandara <ireshab799@gmail.com>
2026-01-20 12:04:31 +0530

    Create header.php

---
+++ header.php
@@ -0,0 +1,21 @@
+<header>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+<link rel="stylesheet" href="header.css">
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="appointments.php">Appointments</a></li>
+            <li><a href="doctors.php">Doctors</a></li>
+            <li><a href="services.php">Services</a></li>
+            <li><a href="medical_records.php">Medical Records</a></li>
+            <li><a href="payments.php">Payments</a></li>
+            <li><a href="login.php">Staff Portal</a></li>
+            <li>
+            <form action="search.php" method="GET">
+                <input type="text" name="query" placeholder="Search services or doctors..." required>
+                <button type="submit">Search</button>
+            </form>
+        </li>
+        </ul>
+    </nav>
+</header>
